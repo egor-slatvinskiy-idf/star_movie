e17d03e5db560d6295f37a5ec2b69e156a6b16d3
egor-slatvinskiy-idf <111364799+egor-slatvinskiy-idf@users.noreply.github.com>
2022-10-28 20:07:29 +0300

    Update main_prod.dart

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -3,7 +3,6 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:presentation/my_app.dart';
 import 'package:star_movie_idf/di/app_injector.dart';
-import 'package:star_movie_idf/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
